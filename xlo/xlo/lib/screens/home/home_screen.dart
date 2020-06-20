98d659b1687c494201fa9aa5f495a65c97a85731
Maximillian Xavier <maximillianxavier@wizsolucoes.com.br>
2020-06-20 11:34:42 -0300

    Adição de botoes Categoria e Filtros

--- xlo/xlo/lib/screens/home/home_screen.dart
+++ xlo/xlo/lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:xlo/blocs/home_bloc.dart';
 import 'package:xlo/common/custom_drawer/custom_drawer.dart';
 import 'package:xlo/screens/home/widgets/search_dialog.dart';
+import 'package:xlo/screens/home/widgets/top_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -84,6 +85,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       drawer: CustomDrawer(),
+      body: Column(
+        children: <Widget>[
+          TopBar(),
+        ],
+      ),
     );
   }
 
