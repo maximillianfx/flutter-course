54d5246ac56a26ca9e16d878f36f05a3f80cae01
Maximillian Xavier <maximillianxavier@wizsolucoes.com.br>
2020-06-20 16:20:17 -0300

    Adição de estado para o botao

--- xlo/xlo/lib/blocs/login/login_bloc.dart
+++ xlo/xlo/lib/blocs/login/login_bloc.dart
@@ -1,11 +1,13 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:xlo/blocs/login/field_state.dart';
+import 'package:xlo/blocs/login/login_bloc_state.dart';
 import 'package:xlo/validators/login_validators.dart';
 
 class LoginBloc with LoginValidator {
 
   final BehaviorSubject<String> _emailController = BehaviorSubject<String>();
   final BehaviorSubject<String> _passwordController = BehaviorSubject<String>();
+  final BehaviorSubject<LoginBlocState> _stateController = BehaviorSubject<LoginBlocState>.seeded(LoginBlocState(LoginState.IDLE));
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
@@ -13,9 +15,12 @@ class LoginBloc with LoginValidator {
   Stream<FieldState> get outEmail => _emailController.stream.transform(emailValidator);
   Stream<FieldState> get outPassword => _passwordController.stream.transform(passwordValidator);
 
+  Stream<LoginBlocState> get outState => _stateController.stream;
+
   void dispose() {
     _emailController.close();
     _passwordController.close();
+    _stateController.close();
   }
 
 }
