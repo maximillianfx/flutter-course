d0ba23a2a033ba8a5fcba29b83a88968abf81617
Maximillian Xavier <maximillianxavier@wizsolucoes.com.br>
2020-06-20 13:27:47 -0300

    Finalização de componentes para tela de filtragem

--- xlo/xlo/lib/screens/filter/filter_screen.dart
+++ xlo/xlo/lib/screens/filter/filter_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xlo/models/filter.dart';
+import 'package:xlo/screens/filter/widgets/animated_button.dart';
 import 'package:xlo/screens/filter/widgets/order_by_field.dart';
 import 'package:xlo/screens/filter/widgets/price_range_field.dart';
 import 'package:xlo/screens/filter/widgets/section_title.dart';
@@ -13,10 +14,19 @@ class FilterScreen extends StatefulWidget {
 class _FilterScreenState extends State<FilterScreen> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final ScrollController _scrollController = ScrollController();
+  Filter _filter = Filter(
+    maxPrice: 100,
+    minPrice: 10,
+    vendorType: VENDOR_TYPE_PARTICULAR,
+    orderBy: OrderBy.PRICE
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         elevation: 0,
         title: const Text("Filtrar busca")
@@ -26,26 +36,53 @@ class _FilterScreenState extends State<FilterScreen> {
           Form(
             key: _formKey,
             child: ListView(
+              controller: _scrollController,
               padding: const EdgeInsets.all(16),
               children: <Widget>[
                 const SectionTitle(title: "Ordenar por"),
                 OrderByField(
-                  initialValue: OrderBy.DATE,
+                  initialValue: _filter.orderBy,
                   onSaved: (v) {
-
+                    _filter.orderBy = v;
                   },
                 ),
                 const SectionTitle(title: "Preço (R\$)"),
-                PriceRangeField(),
+                PriceRangeField(
+                  filter: _filter,
+                ),
                 const SectionTitle(title: "Tipo de anunciante"),
                 VendorTypeField(
-                  initialValue: VENDOR_TYPE_PARTICULAR,
+                  initialValue: _filter.vendorType,
                   onSaved: (v) {
-
+                    _filter.vendorType = v;
                   },
-                )
+                ),
+                const SizedBox(height: 400,)
               ],
             ),
+          ),
+          AnimatedButton(
+            scrollController: _scrollController,
+            onTap: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                if (_filter.maxPrice != null && _filter.minPrice != null) {
+                  if (_filter.minPrice > _filter.maxPrice) {
+                    _scaffoldKey.currentState.showSnackBar(
+                      SnackBar(
+                        content: const Text(
+                          "Faixa de preço inválida",
+                        ),
+                        backgroundColor: Colors.pink,
+                      )
+                    );
+                    return;
+                  }
+                }
+
+                //Salvar dados e pesquisar anuncios
+              }
+            },
           )
         ],
       ),
