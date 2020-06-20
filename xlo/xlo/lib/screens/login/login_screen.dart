2fe1e45dd3cb8db87d2963f16c6ef6847663cf17
Maximillian Xavier <maximillianxavier@wizsolucoes.com.br>
2020-06-20 15:37:17 -0300

    Adição de campos para tela de login

---
+++ xlo/xlo/lib/screens/login/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text(
+          "Entrar"
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0,bottom: 11),
+                child: Text(
+                  "Acessar com e-mail:",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[900]
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3, bottom: 4),
+                child: Text(
+                  "E-mail",
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+              ),
+              StreamBuilder(
+                builder: (context, snapshot) {
+                  return TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3, bottom: 4, top: 26),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Senha",
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        "Esqueci minha senha",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline
+                        ),
+                      ),
+                      onTap: () {
+                        //Navigator.of(context).push(
+                        //  MaterialPageRoute(
+                        //    builder: (context) => RecoveryScreen()
+                        //  )
+                        //)
+                      },
+                    )
+                  ],
+                ),
+              ),
+              StreamBuilder(
+                builder: (context, snapshot) {
+                  return TextField(
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
